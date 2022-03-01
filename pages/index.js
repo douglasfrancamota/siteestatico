2f4b914b1c30cc071fef9da2468c6a6707b9856b
Douglas França Mota <99363537+douglasfrancamota@users.noreply.github.com>
2022-03-01 00:53:15 -0300

    Criando componentes estaticos

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,44 @@
-import Link from 'next/link';
+import Link from "next/link"
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+import RecipeCard from "../components/RecipeCard/RecipeCard";
 
 export default function Index(){
   return(
     <div>
            
-     <Header title= 'Viajando pelo Mundo'/>
+    <Header title= 'Viajando pelo Mundo'/>
 
-      <main>
-         <figure className='recipe-card'>
-      <Link href="/Viagens/Internacionais/Irlanda">
-            <a>
-              <img className='recipe-picture' src='https://www.asseguresuaviagem.com/images/Viagem-para-Irlanda.jpg' />
-            </a>
-        </Link>
-            <div className='recipe-category'>Internacionais</div>
-                <figcaption className='recipe-name'>Irlanda</figcaption>
-          </figure>
-       
-          <figure className='recipe-card'>
-      <Link href="/Viagens/Internacionais/Irlanda">
-            <a>
-              <img className='recipe-picture' src='https://www.asseguresuaviagem.com/images/Viagem-para-Irlanda.jpg' />
-            </a>
-        </Link>
-            <div className='recipe-category'>Internacionais</div>
-                <figcaption className='recipe-name'>Irlanda</figcaption>
-          </figure>
-
-            <figure className='recipe-card'>
-      <Link href="/Viagens/Internacionais/Irlanda">
-            <a>
-              <img className='recipe-picture' src='https://www.asseguresuaviagem.com/images/Viagem-para-Irlanda.jpg' />
-            </a>
-        </Link>
-            <div className='recipe-category'>Internacionais</div>
-                <figcaption className='recipe-name'>Irlanda</figcaption>
-          </figure>         
+    <main>
+      <RecipeCard 
+        name= "Irlanda"
+        category= "Internacional"
+        picture= 'https://www.asseguresuaviagem.com/images/Viagem-para-Irlanda.jpg'
+        link= "/Viagens/Internacionais/Irlanda"
+      />
+       <RecipeCard 
+        name= "Estados Unidos"
+        category= "Internacional"
+        picture= 'https://dicasnovayork.com.br/wp-content/uploads/2017/03/hospedagem_header-1000x700.jpg'
+        link= "/Viagens/Internacionais/Estados-Unidos"
+      />  
+       <RecipeCard 
+        name= "França"
+        category= "Internacional"
+        picture= 'https://media.tacdn.com/media/attractions-splice-spp-674x446/06/74/aa/fc.jpg'
+        link= "/Viagens/Internacionais/Franca"
+      />
+       <RecipeCard 
+        name= "Portugal"
+        category= "Internacional"
+        picture= 'https://i0.wp.com/www.eurodicas.com.br/wp-content/uploads/2018/12/regioes-de-portugal.jpg?w=1360&ssl=1'
+        link= "/Viagens/Internacionais/Franca"
+      />
+                   
               
+        </main>
 
-          
-
-      </main>
-
-      <Footer />
+    <Footer />
       
       
     </div>
