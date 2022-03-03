3592e66c868e6039afe7590b68da404f9f40295b
Douglas França Mota <99363537+douglasfrancamota@users.noreply.github.com>
2022-03-03 00:45:32 -0300

    finalizando pagina irlanda

--- components/Recipe/Recipe.js
+++ components/Recipe/Recipe.js
@@ -12,23 +12,28 @@ export default function Recebe(props){
                         <img className="recipe-body-picture" alt={props.name} src= {props.picture} />    
                         <div>
                         <h2>Sobre o país <br/> </h2> 
-                        <p> A Irlanda[4] (em irlandês: Éire; [ˈeːɾʲə] (Sobre este somescutar 
-                            (ajuda·info)) e em inglês: Ireland [ˈaɪərlənd], localmente: [ˈaːɹlənd] ou 
-                            [ˈaɪɹlənd]), por vezes chamada (de maneira não oficial, para diferenciá-la 
-                            da ilha de mesmo nome, que divide com a Irlanda do Norte) República da Irlanda 
-                            (em irlandês: Poblacht na hÉireann e em inglês: Republic of Ireland) ou então 
-                            Eire,[4] é um estado soberano da Europa que ocupa cerca de cinco sextos da ilha homônima.
-                            </p>
+                        <p>{props.texto}</p>
                         <h2> Locais para visitar </h2>
-                        <p>Clássica nas listas de cidades para visitar dos viajantes, não tem como Dublin ficar de fora 
-                            de qualquer aventura irlandesa. Você tem a cervejaria original da Guinness, o Trinity College,a 
-                            Spire of Dublin, o castelo de Dublin, só para apontar alguns pontos de interesse, sem mencionar
-                                que a cidade inteira é repleta de história e caráter. Se você realmente quer sentir a vibe dessa 
-                                animada cidade, a explore de noite; as ruas ganham vida logo que o sol se põe. Visite a área do 
-                                Temple Bar (o Temple Bar é mais do que só um bar) para boas cervejas, uma galera internacional, 
-                                e vá de bar em bar como jamais fez antes.
-                            </p>
-                            
+                        <h3>{props.subtitulo} </h3>
+                        <img className="recipe-body-picture" alt={props.name} src= {props.picture1} />
+                        <p>{props.texto1}</p>
+
+                        <h3>{props.subtitulo1} </h3>
+                        <img className="recipe-body-picture" alt={props.name} src= {props.picture2} />
+                        <p>{props.texto2}</p>
+                        <h3>{props.subtitulo2} </h3>
+                        <img className="recipe-body-picture" alt={props.name} src= {props.picture3} />
+                        <p>{props.texto3}</p>
+                        <h3>{props.subtitulo3} </h3>
+                        <img className="recipe-body-picture" alt={props.name} src= {props.picture4} />
+                        <p>{props.texto4}</p>
+                        <h3>{props.subtitulo4} </h3>
+                        <img className="recipe-body-picture" alt={props.name} src= {props.picture5} />
+                        <p>{props.texto5}</p>
+
+
+
+
                         </div>
                 </article>
 
--- pages/Viagens/Internacionais/Irlanda.js
+++ pages/Viagens/Internacionais/Irlanda.js
@@ -6,6 +6,23 @@ export default function Irlanda(){
             <Recebe 
                 name="Irlanda"
                 picture="https://www.asseguresuaviagem.com/images/Viagem-para-Irlanda.jpg"
+                texto="A Irlanda é um estado soberano da Europa que ocupa cerca de cinco sextos da ilha homônima. É uma república constitucional governada como uma democracia parlamentar, com um presidente eleito para servir como chefe de Estado. Considerada um país desenvolvido, a Irlanda tem o terceiro maior Índice de Desenvolvimento Humano (IDH) do mundo, além de ótimas classificações em índices que medem o grau de democracia e liberdades como a de imprensa, econômica e política. Sua capital é Dublin e a população do país é estimada em 4,8 milhões de habitantes."
+                subtitulo="1- Castelo de Dublin"
+                picture1="https://upstudy.com.br/wp-content/uploads/2021/03/castelo-de-dublin.jpg"
+                texto1="O primeiro que iremos conhecer é o Castelo de Dublin o qual foi construído no século 13 e está localizado na capital da Irlanda, ele já serviu como sede do governo britânico, fortaleza militar, sede do Tribunal de Justiça Irlandês entre outras funções."
+                subtitulo1="2- Catedral de St. Patrick"
+                picture2="https://upstudy.com.br/wp-content/uploads/2021/03/catedral-st-patrick-dublin.jpg"
+                texto2="Uma das maiores e mais importantes igrejas do país, a Catedral de St. Patrick  foi fundada em 1191. Ela também fica na cidade de Dublin e já recebeu pessoas muito importantes, como  William of Orange, Rei James I, a Rainha Victoria e seu marido e também o Príncipe Albert."
+                subtitulo2="3- Castelo de Bunratty"
+                picture3="https://upstudy.com.br/wp-content/uploads/2021/03/castelo-de-bunratty.jpg"
+                texto3="Para quem vai estar nos arredores de Shannon, a visita ao Castelo de Bunratty deve sem dúvidas ser incluída no roteiro. Construído em 1421, o local fica em Bunratty, Claire e já foi habitado pelos vikings, nômades, condes e lordes. "
+                subtitulo3="4- The Wild Atlantic Way"
+                picture4="https://upstudy.com.br/wp-content/uploads/2021/03/irlanda-wild-atlantic-way.jpg"
+                texto4="A famosa rota Wild Atlantic Way é um passeio incrível que proporciona por todo o percurso paisagens e lugares maravilhosos. São cerca de 2.500 km de extensão beirando o Oceano Atlântico, passando por ilhas, cidadezinhas e vistas de tirar o fôlego. Listamos alguns dos lugares que você vai se deparar ao longo da rota e que merecem uma parada."
+                subtitulo4="5- The Ring of Kerry"
+                picture5="https://upstudy.com.br/wp-content/uploads/2021/03/irlanda-the-ring-of-kerry.jpg"
+                texto5="Uma outra rota imperdível é The Ring of Kerry. Uma estrada circular localizada no condado de Kerry. Com cerca de 179 km, a viagem pode ser feita de carro, bicicleta ou até mesmo a pé."
+              
             />
         </div>
     )
